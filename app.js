6dc05cf579e783d56f090f6942a7e449d9c46afa
VIJAYALAYAN K <122135663+VIJAYALAYAN2002@users.noreply.github.com>
2024-01-26 14:27:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var score = 10;
+var guessnumber = document.getElementById("input");
+var result = document.getElementById("choice");
+var random_num = Math.floor(Math.random() * 10) + 1;
+var scoreElement = document.getElementById("score");
+
+function check() {
+    var enterednumber = parseInt(guessnumber.value);
+
+    if (random_num == enterednumber) {
+        console.log("RIGHT");
+        result.textContent = "RIGHT";
+        alert("YOU WON");
+    } else {
+        score = score - 1;
+        console.log("WRONG");
+        result.textContent = "WRONG";
+        scoreElement.textContent = "Score: " + score;
+
+        if (score <= 0) {
+            alert("You lose");
+        }
+    }
+}
